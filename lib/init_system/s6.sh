d08e0c459937865e03b0cdd75cd93e34ae08f960
W <psezar@proton.me>
2025-07-08 17:30:19 +0300

    Update s6.sh

--- lib/init_system/s6.sh
+++ lib/init_system/s6.sh
@@ -16,46 +16,56 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>
+use std::process::{Command, ExitStatus};
 
+const RED: &str = "\x1b[0;31m";
+const GREEN: &str = "\x1b[0;32m";
+const RESET: &str = "\x1b[0m";
 
-declare -r RED="\033[0;31m"
-declare -r GREEN="\033[0;32m"
-declare -r RESET="\033[0m"
-
-function _run_s6_rc()
+fn execute_s6_rc(action: &str, service: &str) -> std::io::Result<ExitStatus> 
 {
-    local ACTION="$1"
-    local SERVICE="$2"
+    let mut cmd = Command::new("s6-rc");
+    cmd.arg(action);
+    
+    if !service.is_empty() 
+    {
+        cmd.arg(service);
+    }
 
-    if s6-rc "$ACTION" "$SERVICE" >/dev/null 2>&1; then
-        echo -e "${GREEN}[*] Success!${RESET}"
-        return 0
-    else
-        echo -e "${RED}[!] Error: s6-rc $ACTION $SERVICE failed.${RESET}"
-        return 1
-    fi
+    let status = cmd
+        .stdout(std::process::Stdio::null())
+        .stderr(std::process::Stdio::null())
+        .status()?;
+
+    if status.success() 
+    {
+        println!("{}[*] Success!{}", GREEN, RESET);
+    } 
+    else 
+    {
+        println!("{}[!] Error: s6-rc {} {} failed.{}", 
+               RED, action, service, RESET);
+    }
+
+    Ok(status)
 }
 
-function help_s6_rc()     { _run_s6_rc "help"     "$1"; }
-function list_s6_rc()     { _run_s6_rc "list"     "$1"; }
-function listall_s6_rc()  { _run_s6_rc "listall"  "$1"; }
-function diff_s6_rc()     { _run_s6_rc "diff"     "$1"; }
-function start_s6_rc()    { _run_s6_rc "start"    "$1"; }
-function stop_s6_rc()     { _run_s6_rc "stop"     "$1"; }
-function change_s6_rc()   { _run_s6_rc "change"   "$1"; }
-
-function execute_s6_rc()
+macro_rules! s6_rc_command 
 {
-    local COMMAND="$1"
-    local SERVICE="$2"
-    local FUNC_NAME="${COMMAND//-/_}_s6_rc"
-
-    if declare -f "$FUNC_NAME" >/dev/null 2>&1; then
-        $FUNC_NAME "$SERVICE"
-        return $?
-    else
-        echo -e "${RED}[!] Error: Unknown command: $COMMAND${RESET}"
-        return 1
-    fi
+    ($name:ident, $action:literal, $with_service:expr) => 
+    {
+        pub fn $name(service: Option<&str>) -> std::io::Result<ExitStatus> 
+        {
+            execute_s6_rc($action, if $with_service { service.unwrap_or_default() } else { "" })
+        }
+    };
 }
 
+s6_rc_command!(start_s6_rc, "start", true);
+s6_rc_command!(stop_s6_rc, "stop", true);
+s6_rc_command!(change_s6_rc, "change", true);
+
+s6_rc_command!(help_s6_rc, "help", false);
+s6_rc_command!(list_s6_rc, "list", false);
+s6_rc_command!(listall_s6_rc, "listall", false);
+s6_rc_command!(diff_s6_rc, "diff", false);
